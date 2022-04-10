6d77b8e45dd8d5484cc8bba4dc393c8377bad1da
sdediego <sergio.dediego@outlook.com>
2022-04-10 17:38:54 +0200

    master: Add application relational database setup

--- src/infrastructure/settings/development.py
+++ src/infrastructure/settings/development.py
@@ -12,7 +12,11 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'drf_jwt',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
