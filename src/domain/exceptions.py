8bb04ed790643c836da0c3cef2e1e5023ca02907
sdediego <sergio.dediego@outlook.com>
2022-04-11 23:44:24 +0200

    master: Create entity error classes

---
+++ src/domain/exceptions.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+class EntityError(Exception):
+
+    def __init__(self, message: str = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.message = message
+
+    def __str__(self) -> str:
+        return f'{self.__class__.__name__}: {self.message}'
+
+
+class EntityDoesNotExist(EntityError):
+    pass
+
+
+class DuplicateEntity(EntityError):
+    pass
