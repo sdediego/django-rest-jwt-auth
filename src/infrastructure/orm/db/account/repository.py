cc33a237db94d82a77eee34648fe54d45e1963fd
sdediego <sergio.dediego@outlook.com>
2022-04-14 20:52:05 +0200

    master: Create user database repository

---
+++ src/infrastructure/orm/db/account/repository.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+from django.db.utils import IntegrityError
+
+from src.domain.account import UserEntity
+from src.domain.exceptions import DuplicateEntity
+from src.infrastructure.orm.db.account.models import User
+
+
+class UserDatabaseRepository:
+
+    def create(self, email: str, password: str) -> UserEntity:
+        try:
+            user = User.objects.create(email=email, password=password)
+        except IntegrityError as err:
+            raise DuplicateEntity('Already exists a user with this data: %s', str(err))
+        return user.map(fields=['email'])
