4ff3537a481b392b9d661d7349b48d5bdaa60274
sdediego <sergio.dediego@outlook.com>
2022-04-15 16:40:26 +0200

    master: Create user database repository integration tests

---
+++ tests/infrastructure/orm/db/factories.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+from datetime import datetime, timezone
+import string
+
+from factory import django, fuzzy
+
+from src.infrastructure.orm.db.account.models import User
+
+
+class UserFactory(django.DjangoModelFactory):
+
+    class Meta:
+        model = User
+
+    name = fuzzy.FuzzyText(length=20, chars=string.ascii_letters)
+    surname = fuzzy.FuzzyText(length=20, chars=string.ascii_letters)
+    username = fuzzy.FuzzyText(length=20, chars=string.ascii_letters)
+    email = fuzzy.FuzzyText(length=20, chars=string.ascii_letters, suffix='@mail.com')
+    password = fuzzy.FuzzyText(length=20, prefix='Password_1234')
+    is_active = fuzzy.FuzzyChoice([True, False])
+    last_login = fuzzy.FuzzyDateTime(start_dt=datetime.now(tz=timezone.utc))
+    date_joined = fuzzy.FuzzyDateTime(start_dt=datetime.now(tz=timezone.utc))
---
+++ tests/infrastructure/orm/db/integration/test_account_repositories.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+import pytest
+
+from src.domain.account import UserEntity
+from src.domain.exceptions import DuplicateEntity
+from src.infrastructure.orm.db.account.repositories import UserDatabaseRepository
+from tests.fixtures import user
+from tests.infrastructure.orm.db.factories import UserFactory
+
+
+@pytest.mark.django_db
+def test_user_db_repository_create(user):
+    result = UserDatabaseRepository().create(user.email, user.password)
+    assert isinstance(user, UserEntity)
+    assert result.email == user.email
+    assert str(result) != str(user)
+
+
+@pytest.mark.django_db
+def test_user_db_repository_create_duplicate(user):
+    UserFactory.create(email=user.email, password=user.password)
+    with pytest.raises(DuplicateEntity) as err:
+        UserDatabaseRepository().create(user.email, user.password)
+    assert 'Already exists a user with this data' in str(err.value)
