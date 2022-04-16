33500ae8a2d5a011fad0241c3c47a97beb7a8c56
sdediego <sergio.dediego@outlook.com>
2022-04-16 13:29:11 +0200

    master: Add login method to user interactor

--- src/usecases/account.py
+++ src/usecases/account.py
@@ -1,12 +1,15 @@
 # coding: utf-8
 
-from src.domain.account import UserEntity
+from src.domain.account import UserEntity, UserTokenEntity
 
 
 class UserInteractor:
 
     def __init__(self, user_repo: object):
         self.user_repo = user_repo
+
+    def login(self, email: str, password: str) -> UserTokenEntity:
+        return self.user_repo.login(email, password)
 
     def register(self, email: str, password: str) -> UserEntity:
         return self.user_repo.register(email, password)
