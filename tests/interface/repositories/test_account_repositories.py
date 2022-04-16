25fb3e3aaf7f5f4854788af306f7d5cd43170f78
sdediego <sergio.dediego@outlook.com>
2022-04-16 15:15:11 +0200

    master: Add user repository login tests

--- tests/interface/repositories/test_account_repositories.py
+++ tests/interface/repositories/test_account_repositories.py
@@ -4,9 +4,21 @@ from unittest.mock import Mock
 
 import pytest
 
-from src.domain.account import UserEntity
+from src.domain.account import UserEntity, UserTokenEntity
 from src.interface.repositories.account import UserRepository
-from tests.fixtures import user
+from tests.fixtures import user, user_token
+
+
+@pytest.mark.unit
+def test_user_interactor_login(user, user_token):
+    db_repo = Mock()
+    db_repo.get.return_value = user_token
+    user_repo = UserRepository(db_repo)
+    result = user_repo.login(user.email, user.password)
+    assert db_repo.get.called
+    assert isinstance(result, UserTokenEntity)
+    assert result.token == user_token.token
+    assert UserTokenEntity.to_string(result) == UserTokenEntity.to_string(user_token)
 
 
 @pytest.mark.unit
@@ -16,5 +28,6 @@ def test_user_interactor_register(user):
     user_repo = UserRepository(db_repo)
     result = user_repo.register(user.email, user.password)
     assert db_repo.create.called
+    assert isinstance(result, UserEntity)
     assert result.email == user.email
     assert UserEntity.to_string(result) == UserEntity.to_string(user)
