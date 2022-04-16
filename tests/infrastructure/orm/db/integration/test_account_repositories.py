e95f6fb30ef9077e45488635f7d1ca255d05e438
sdediego <sergio.dediego@outlook.com>
2022-04-16 17:47:37 +0200

    master: Add user database repository get integration tests

--- tests/infrastructure/orm/db/integration/test_account_repositories.py
+++ tests/infrastructure/orm/db/integration/test_account_repositories.py
@@ -3,7 +3,7 @@
 import pytest
 
 from src.domain.account import UserEntity
-from src.domain.exceptions import EntityDuplicate
+from src.domain.exceptions import EntityDoesNotExist, EntityDuplicate
 from src.infrastructure.orm.db.account.repositories import UserDatabaseRepository
 from tests.fixtures import user
 from tests.infrastructure.orm.db.factories import UserFactory
@@ -23,3 +23,20 @@ def test_user_db_repository_create_duplicate(user):
     with pytest.raises(EntityDuplicate) as err:
         UserDatabaseRepository().create(user.email, user.password)
     assert 'Already exists a user with this data' in str(err.value)
+
+
+@pytest.mark.django_db
+def test_user_db_repository_get(user):
+    instance = UserFactory.create(
+        email=user.email, password=user.password, last_login=user.last_login)
+    result = UserDatabaseRepository().get(user.email, user.password)
+    assert isinstance(user, UserEntity)
+    assert result.email == user.email
+    assert result.last_login != instance.last_login
+
+
+@pytest.mark.django_db
+def test_user_db_repository_get_does_not_exist():
+    with pytest.raises(EntityDoesNotExist) as err:
+        UserDatabaseRepository().get('user@email.com', 'password')
+    assert 'User does not exist with this data' in str(err.value)
