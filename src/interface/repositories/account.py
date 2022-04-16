7be4d7bc93902423d8f893b6e5c03279aa1684b4
sdediego <sergio.dediego@outlook.com>
2022-04-16 15:14:18 +0200

    master: Add login method to user repository

--- src/interface/repositories/account.py
+++ src/interface/repositories/account.py
@@ -1,12 +1,15 @@
 # coding: utf-8
 
-from src.domain.account import UserEntity
+from src.domain.account import UserEntity, UserTokenEntity
 
 
 class UserRepository:
 
     def __init__(self, db_repo: object):
         self.db_repo = db_repo
+
+    def login(self, email: str, password: str) -> UserTokenEntity:
+        return self.db_repo.get(email, password)
 
     def register(self, email: str, password: str) -> UserEntity:
         return self.db_repo.create(email, password)
