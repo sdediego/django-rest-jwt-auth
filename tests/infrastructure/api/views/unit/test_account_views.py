96d1290ea5f47e9dae9db165c55f3de22f04f2ac
sdediego <sergio.dediego@outlook.com>
2022-04-16 17:55:09 +0200

    master: Add user api views login unit tests

--- tests/infrastructure/api/views/unit/test_account_views.py
+++ tests/infrastructure/api/views/unit/test_account_views.py
@@ -8,7 +8,24 @@ from django.test.client import RequestFactory
 import pytest
 
 from src.infrastructure.api.views.account import UserViewSet
-from tests.fixtures import user
+from tests.fixtures import user, user_token
+
+
+@pytest.mark.unit
+def test_user_viewset_login(user_token):
+    request = RequestFactory()
+    request.data = Mock()
+    viewset = UserViewSet()
+    viewset.viewset_factory = Mock()
+    mock_factory_create = viewset.viewset_factory.create
+    mock_factory_create.return_value = Mock()
+    mock_controller_login = mock_factory_create.return_value.login
+    mock_controller_login.return_value = (vars(user_token), HTTPStatus.OK.value)
+    response = viewset.login(request)
+    assert hasattr(response, 'status_code')
+    assert response.status_code == HTTPStatus.OK.value
+    assert hasattr(response, 'data')
+    assert isinstance(response.data, dict)
 
 
 @pytest.mark.unit
@@ -20,9 +37,9 @@ def test_user_viewset_register(user):
     mock_factory_create = viewset.viewset_factory.create
     mock_factory_create.return_value = Mock()
     mock_controller_register = mock_factory_create.return_value.register
-    mock_controller_register.return_value = (vars(user), HTTPStatus.OK.value)
+    mock_controller_register.return_value = (vars(user), HTTPStatus.CREATED.value)
     response = viewset.register(request)
     assert hasattr(response, 'status_code')
-    assert response.status_code == HTTPStatus.OK.value
+    assert response.status_code == HTTPStatus.CREATED.value
     assert hasattr(response, 'data')
     assert isinstance(response.data, dict)
