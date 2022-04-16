88f7348927293174bd0e639f928e6140f70775b2
sdediego <sergio.dediego@outlook.com>
2022-04-16 18:03:12 +0200

    master: Add user api views login integration tests

--- tests/infrastructure/api/views/integration/test_account_views.py
+++ tests/infrastructure/api/views/integration/test_account_views.py
@@ -11,6 +11,64 @@ from src.infrastructure.orm.db.account.models import User
 from tests.fixtures import user
 
 
+@patch.object(User, 'save')
+@patch.object(User, 'objects')
+def test_user_viewset_login(mock_objects, mock_save, user, client):
+    mock_get = mock_objects.get
+    mock_get.return_value = User(**dataclasses.asdict(user))
+    url = reverse('api:accounts-login')
+    data = {
+        'email': user.email,
+        'password': user.password
+    }
+    response = client.post(url, data=data)
+    assert mock_get.called
+    assert mock_save.called
+    assert hasattr(response, 'status_code')
+    assert response.status_code == HTTPStatus.OK.value
+    assert hasattr(response, 'data')
+    assert isinstance(response.data, dict)
+
+
+@patch.object(User, 'save')
+@patch.object(User, 'objects')
+def test_user_viewset_login_bad_request(mock_objects, mock_save, user, client):
+    mock_get = mock_objects.get
+    mock_get.return_value = User(**dataclasses.asdict(user))
+    url = reverse('api:accounts-login')
+    data = {
+        'email': user.email
+    }
+    response = client.post(url, data=data)
+    assert not mock_get.called
+    assert not mock_save.called
+    assert hasattr(response, 'status_code')
+    assert response.status_code == HTTPStatus.BAD_REQUEST.value
+    assert hasattr(response, 'data')
+    assert isinstance(response.data, dict)
+    assert 'errors' in response.data
+
+
+@patch.object(User, 'save')
+@patch.object(User, 'objects')
+def test_user_viewset_login_does_not_exist(mock_objects, mock_save, user, client):
+    mock_get = mock_objects.get
+    mock_get.side_effect = User.DoesNotExist
+    url = reverse('api:accounts-login')
+    data = {
+        'email': user.email,
+        'password': user.password
+    }
+    response = client.post(url, data=data)
+    assert mock_get.called
+    assert not mock_save.called
+    assert hasattr(response, 'status_code')
+    assert response.status_code == HTTPStatus.BAD_REQUEST.value
+    assert hasattr(response, 'data')
+    assert isinstance(response.data, dict)
+    assert 'error' in response.data
+
+
 @patch.object(User, 'objects')
 def test_user_viewset_register(mock_objects, user, client):
     mock_create = mock_objects.create
