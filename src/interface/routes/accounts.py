72413528dcdd03eab75baabdf2bf2452ce36934f
sdediego <sergio.dediego@outlook.com>
2022-04-17 17:19:22 +0200

    master: Add refresh route to user router

--- src/interface/routes/accounts.py
+++ src/interface/routes/accounts.py
@@ -1,12 +1,19 @@
 # coding: utf-8
 
 from src.interface.controllers.account import UserController
-from src.interface.routes.core.constants import HTTP_VERB_POST
+from src.interface.routes.core.constants import HTTP_VERB_GET, HTTP_VERB_POST
 from src.interface.routes.core.routing import Route, Router
 
 
 user_router = Router()
 user_router.register([
+    Route(
+        http_verb=HTTP_VERB_POST,
+        path=r'^accounts/users/login/$',
+        controller=UserController,
+        method='login',
+        name='user_login'
+    ),
     Route(
         http_verb=HTTP_VERB_POST,
         path=r'^accounts/users/register/$',
@@ -15,10 +22,10 @@ user_router.register([
         name='user_register'
     ),
     Route(
-        http_verb=HTTP_VERB_POST,
-        path=r'^accounts/users/login/$',
+        http_verb=HTTP_VERB_GET,
+        path=r'^accounts/users/token/refresh/$',
         controller=UserController,
-        method='login',
-        name='user_login'
+        method='refresh',
+        name='token_refresh'
     )
 ])
